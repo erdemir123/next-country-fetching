f542a4f4ce059945eeb2b64dccb3f0c8c686d7f8
erdemir123 <sadikerdemir93@gmail.com>
2023-01-05 21:11:49 +0300

    next useTheme added

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Providers from '../components/providers'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,23 @@
+"use client"
+import {useTheme} from "next-themes"
+import Link from "next/link";
 
 
 export default function Home() {
+  const {theme,setTheme}=useTheme()
+  console.log(theme);
   return (
-    <div className="text-3xl font-bold underline text-slate-600">sadık</div>
+    <>
+     <div className="text-3xl font-bold underline text-slate-600">sadık</div>
+     <div>
+      The current theme is: {theme} <br />
+      <button onClick={() => setTheme('light')}>Light Mode</button>
+      <br />
+      <button onClick={() => setTheme('dark')}>Dark Mode</button>
+      <Link href="/hakkimizda">sadık</Link>
+    </div>
+    
+    </>
+    
   )
 }
